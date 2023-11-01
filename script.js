72099699073e32fa47da2e812766d239269d612a
tinoviMVP <sereda200524@gmail.com>
2023-11-01 13:39:58 +0500

    cnhfybws 1-2

---
+++ script.js
@@ -0,0 +1,17 @@
+let myName = "Natalya"
+let surName = "Sereda"
+let middleName = "Aleksandrovna"
+let  Age = 17
+let city = "Tashkent"
+let country = "Uzbekistan"
+let  houseNumber= "8"
+let Cellphone = 998461335
+let StreamNumber = "45"
+let randomNumber = "66"
+let yearOfBirth = "2005"
+let userNameInTg = "@tinovi"
+let areThereAnyBrother= "false"
+let areThereAnySister= "true"
+console.log(StreamNumber + Age)
+(yearOfBirth + houseNumber)
+console.log(myName + Age)
